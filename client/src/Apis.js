e136085a6202a0c268b7a5bb7f84279edd1df6ce
Aisha60 <ayeshaefgh60@gmail.com>
2024-08-12 13:13:56 +0500

    user registration completed

--- client/src/Apis.js
+++ client/src/Apis.js
@@ -1,11 +1,15 @@
 import Axios from "axios";
 
 export const API = Axios.create({
-    baseURL:"http://localhost:3000"
+    baseURL:"http://localhost:5000"
 })
 
-// creating  apis for different functions 
+// creating  apis for Admin
 export const loginAdmin = (data) => API.post('admin/login' , data);
 export const showPendingProperties = ()=> API.get('admin/view_pending');
 export const approve = (id)=> API.patch(`admin/approve_pending/${id}`);
 export const reject = (id)=> API.patch(`admin/reject_pending/${id}`);
+
+// creating apis for user
+export const loginUser = (data) => API.post('users/login' , data);
+export const signupUser = (data) => API.post('users/signup' , data);
